f5d8074a66bd54e2f0c29e187cad18ecec45c6bf
Andrew Schoewe <anschoewe@gmail.com>
2012-11-11 18:35:49 -0500

    Initial commit

---
+++ schaytell/src/com/schoewe/SchaytellServlet.java
@@ -0,0 +1,15 @@
+package com.schoewe;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class SchaytellServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException
+	{
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
